32c21c55d6a49053543302a9fc85afc28b7444bc
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-10-08 22:08:55 +0000

    auto commit 6       20241008 220842

--- src/rpart/acr227_ene_montecarlo.r
+++ src/rpart/acr227_ene_montecarlo.r
@@ -8,8 +8,14 @@ require("primes")
 
 
 PARAM <- list()
+# reemplazar por las propias semillas
+#PARAM$semilla_primigenia <- 102191
+PARAM$semilla_primigenia <- 892249
+PARAM$qsemillas <- 50
 
-PARAM$qsemillas <- 200
+# elegir SU dataset comentando/ descomentando
+PARAM$dataset_nom <- "~/datasets/vivencial_dataset_pequeno.csv"
+# PARAM$dataset_nom <- "~/datasets/conceptual_dataset_pequeno.csv"
 
 PARAM$training_pct <- 70L  # entre  1L y 99L 
 
