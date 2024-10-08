91aa26813da4723eea37b20994c20f3095fe65bb
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-10-08 22:04:30 +0000

    auto commit 3       20241008 220420

--- src/ambiente/301_GrabarMiAmbiente.r
+++ src/ambiente/301_GrabarMiAmbiente.r
@@ -7,18 +7,18 @@ require("yaml")
 miAmbiente <- list()
 
 miAmbiente$modalidad <-  "conceptual"
-# miAmbiente$modalidad <-  "vivencial"
+# miAmbiente$modalidad <-  "conceptual"
 
 miAmbiente$dataset_pequeno <- "~/datasets/conceptual_dataset_pequeno.csv"
-# miAmbiente$dataset_pequeno <- "~/datasets/vivencial_dataset_pequeno.csv"
+# miAmbiente$dataset_pequeno <- "~/datasets/conceptual_dataset_pequeno.csv"
 
 
 miAmbiente$dataset_competencia <- "~/datasets/conceptual_competencia_2024.csv.gz"
-# miAmbiente$dataset_competencia <- "~/datasets/vivencial_competencia_2024.csv.gz"
+# miAmbiente$dataset_competencia <- "~/datasets/conceptual_competencia_2024.csv.gz"
 
 
 # aqui va su primer semilla
-miAmbiente$semilla_primigenia <- 523801L
+miAmbiente$semilla_primigenia <- 100069L
 
 
 write_yaml( miAmbiente,
@@ -31,6 +31,6 @@ dir.create("~/buckets/b1/repos/", showWarnings = FALSE)
 dir.create("~/buckets/b1/repos/labo2024ba", showWarnings = FALSE)
 
 cat("Espere unos minutos mientras se hace un backup\n")
-system( "~/install/repobrutalcopy.sh", wait= FALSE )
+system( "~/install/repobrutalcopy.sh" )
 
 cat("\nel ambiente ha sido GRABADO\n")
