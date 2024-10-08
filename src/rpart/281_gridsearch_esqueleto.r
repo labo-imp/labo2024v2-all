b697590124e60ff1e67aefcb45cc3b97bfce3570
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-10-08 21:30:41 +0000

    auto commit 4       20241008 213031

--- src/rpart/281_gridsearch_esqueleto.r
+++ src/rpart/281_gridsearch_esqueleto.r
@@ -13,7 +13,7 @@ require("primes")
 PARAM <- list()
 # reemplazar por su primer semilla
 PARAM$semilla_primigenia <- 354877
-PARAM$qsemillas <- 20
+PARAM$qsemillas <- 40
 
 PARAM$training_pct <- 70L  # entre  1L y 99L 
 
@@ -142,16 +142,18 @@ tb_grid_search_detalle <- data.table(
 
 # itero por los loops anidados para cada hiperparametro
 
-for (vmax_depth in c(4, 6, 8, 10, 12, 14)) {
-  for (vmin_split in c(1000, 800, 600, 400, 200, 100, 50, 20, 10)) {
+for (vmax_depth in c(4, 6, 7, 8, 9, 10, 12, 14)) {
+  for (vmin_split in c(1000, 800, 600, 400, 200, 100, 50, 20, 10, 8, 7, 4)) {
+    for (vmin_cp in c(-0.4, 0.0, -0.55)){
+      for(vmin_bucket in c(100, 200, 500)){
     # notar como se agrega
 
     # vminsplit  minima cantidad de registros en un nodo para hacer el split
     param_basicos <- list(
-      "cp" = -0.45, # complejidad minima
+      "cp" = vmin_cp, # complejidad minima
       "maxdepth" = vmax_depth, # profundidad máxima del arbol
       "minsplit" = vmin_split, # tamaño minimo de nodo para hacer split
-      "minbucket" = 35 # minima cantidad de registros en una hoja
+      "minbucket" = vmin_bucket # minima cantidad de registros en una hoja
     )
 
     # Un solo llamado, con la semilla 17
@@ -162,7 +164,8 @@ for (vmax_depth in c(4, 6, 8, 10, 12, 14)) {
       list( tb_grid_search_detalle,
             rbindlist(ganancias) )
     )
-
+      }
+    }
   }
 
   # grabo cada vez TODA la tabla en el loop mas externo
