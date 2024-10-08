31eeb7a4fa4c0085a3cd53eace00f8c5375e2537
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-10-08 20:35:21 +0000

    auto commit 1       20241008 203507

--- src/rpart/222_traintest_montecarlo.r
+++ src/rpart/222_traintest_montecarlo.r
@@ -7,7 +7,7 @@ require("parallel")
 
 PARAM <- list()
 # reemplazar por las propias semillas
-PARAM$semillas <- c(100003, 100019, 100043, 100049, 100057)
+PARAM$semillas <- c(102191, 200177, 410551, 552581, 892237)
 
 # elegir SU dataset comentando/ descomentando
 PARAM$dataset_nom <- "~/datasets/vivencial_dataset_pequeno.csv"
