38c57f143d2f47ba44a926fa2aa90600b20254dc
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-10-24 19:13:36 +0000

    auto commit 7       20241024 191325

--- src/lightgbm/422_lightgbm_binaria_BO.r
+++ src/lightgbm/422_lightgbm_binaria_BO.r
@@ -60,9 +60,9 @@ hs <- makeParamSet(
   makeNumericParam("min_gain_to_split", lower = 0, upper = 0.1),
   makeNumericParam("lambda_l1", lower = 0, upper = 10),
   makeNumericParam("lambda_l2", lower = 0, upper = 10),
-  makeIntegerParam("bagging_freq", lower = 0L, upper = 10L),
-  makeIntegerParam("max_depth", lower = 1L, upper = 20L),     
-  makeNumericParam("path_smooth", lower = 0, upper = 10)      
+  makeIntegerParam("bagging_freq", lower = 0L, upper = 5L),
+  makeNumericParam("path_smooth", lower = 0.0, upper = 1.0),
+  makeIntegerParam("max_depth", lower = -1L, upper = 30L)
 )
 
 #------------------------------------------------------------------------------
