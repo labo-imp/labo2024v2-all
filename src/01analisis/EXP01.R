a0694352c555040e566a5aee73e4650b013b0f54
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-10-29 19:59:17 +0000

    auto commit 7       20241029 195907

--- src/01analisis/EXP01.R
+++ src/01analisis/EXP01.R
@@ -27,3 +27,47 @@ ganancia_1 %>% group_by(semilla) %>% count()
 ganancia_1 %>% arrange(desc(ganancia))
 
 wilcox.test(ganancia$ganancia,ganancia_1$ganancia, paired = TRUE)
+
+# Assuming df is your data frame name
+df_summary <- ganancia %>%
+  group_by(corte) %>%
+  summarize(
+    min_ganancia = min(ganancia),
+    med_ganancia = median(ganancia),
+    max_ganancia = max(ganancia)
+  )
+
+df_summary
+
+
+ganancia %>%
+  left_join(df_summary, by = "corte") %>% 
+  ggplot(aes(x = corte, y = ganancia)) +
+  geom_jitter(color = "grey", alpha = 0.6) + # Puntos individuales
+  geom_point(aes(y = med_ganancia), color = "black", size = 3) +  # Punto para la mediana
+  geom_errorbar(aes(ymin = min_ganancia, ymax = max_ganancia), color = "black") + # Barras de error
+  labs(x = "Corte", y = "Ganancia", title = "Distribución de Ganancia por Corte") +
+  theme_minimal()
+
+
+# Assuming df is your data frame name
+df_summary_1 <- ganancia_1%>%
+  group_by(corte) %>%
+  summarize(
+    min_ganancia = min(ganancia),
+    med_ganancia = median(ganancia),
+    max_ganancia = max(ganancia)
+  )
+
+df_summary_1
+
+
+ganancia_1 %>%
+  left_join(df_summary_1, by = "corte") %>% 
+  ggplot(aes(x = corte, y = ganancia)) +
+  geom_jitter(color = "grey", alpha = 0.6) + # Puntos individuales
+  geom_point(aes(y = med_ganancia), color = "black", size = 3) +  # Punto para la mediana
+  geom_errorbar(aes(ymin = min_ganancia, ymax = max_ganancia), color = "black") + # Barras de error
+  labs(x = "Corte", y = "Ganancia", title = "Ganancia por Corte - SIN MESES Pandemia") +
+  theme_minimal()
+
