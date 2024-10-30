38ced47a6be3012c7b2a9ba1e63530e9dd0a9142
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-10-30 02:17:51 +0000

    auto commit 8       20241030 021741

--- src/01analisis/EXP01.R
+++ src/01analisis/EXP01.R
@@ -46,7 +46,8 @@ ganancia %>%
   geom_jitter(color = "grey", alpha = 0.6) + # Puntos individuales
   geom_point(aes(y = med_ganancia), color = "black", size = 3) +  # Punto para la mediana
   geom_errorbar(aes(ymin = min_ganancia, ymax = max_ganancia), color = "black") + # Barras de error
-  labs(x = "Corte", y = "Ganancia", title = "Distribución de Ganancia por Corte") +
+  scale_x_continuous(breaks = seq(1400, 2600, by = 200)) +
+  labs(x = "Corte", y = "Ganancia", title = "Ganancia por Corte - CON MESES Pandemia") +
   theme_minimal()
 
 
@@ -68,6 +69,7 @@ ganancia_1 %>%
   geom_jitter(color = "grey", alpha = 0.6) + # Puntos individuales
   geom_point(aes(y = med_ganancia), color = "black", size = 3) +  # Punto para la mediana
   geom_errorbar(aes(ymin = min_ganancia, ymax = max_ganancia), color = "black") + # Barras de error
+  scale_x_continuous(breaks = seq(1400, 2600, by = 200)) +
   labs(x = "Corte", y = "Ganancia", title = "Ganancia por Corte - SIN MESES Pandemia") +
   theme_minimal()
 
