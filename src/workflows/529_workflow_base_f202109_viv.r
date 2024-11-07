c08590b55e7f34a2b6633a96e2218b6b4d45f570
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-11-07 02:17:42 +0000

    auto commit 1       20241107 021735

--- src/workflows/529_workflow_base_f202109_viv.r
+++ src/workflows/529_workflow_base_f202109_viv.r
@@ -355,15 +355,15 @@ HT_tuning_base <- function( pinputexps, bo_iteraciones, bypass=FALSE)
     bagging_fraction = 1.0, # 0.0 < bagging_fraction <= 1.0
     pos_bagging_fraction = 1.0, # 0.0 < pos_bagging_fraction <= 1.0
     neg_bagging_fraction = 1.0, # 0.0 < neg_bagging_fraction <= 1.0
+    is_unbalance = FALSE, #
     scale_pos_weight = 1.0, # scale_pos_weight > 0.0
 
     drop_rate = 0.1, # 0.0 < neg_bagging_fraction <= 1.0
     max_drop = 50, # <=0 means no limit
     skip_drop = 0.5, # 0.0 <= skip_drop <= 1.0
+
     extra_trees = FALSE,
-    
     # Parte variable
-    is_unbalance = TRUE,
     learning_rate = c( 0.02, 0.3 ),
     feature_fraction = c( 0.5, 0.9 ),
     num_leaves = c( 8L, 2048L,  "integer" ),
