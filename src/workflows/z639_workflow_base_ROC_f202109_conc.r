61774905f2df0a5139ff156492ba77c7e7b7fc03
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-11-09 19:15:24 +0000

    auto commit 54       20241109 191513

--- src/workflows/z639_workflow_base_ROC_f202109_conc.r
+++ src/workflows/z639_workflow_base_ROC_f202109_conc.r
@@ -12,8 +12,8 @@ if( !exists("envg") ) envg <- env()  # global environment
 
 envg$EXPENV <- list()
 envg$EXPENV$bucket_dir <- "~/buckets/b1"
-envg$EXPENV$exp_dir <- "~/buckets/b1/expw13/"
-envg$EXPENV$wf_dir <- "~/buckets/b1/flow13/"
+envg$EXPENV$exp_dir <- "~/buckets/b1/expw131/"
+envg$EXPENV$wf_dir <- "~/buckets/b1/flow131/"
 envg$EXPENV$repo_dir <- "~/labo2024v2/"
 envg$EXPENV$datasets_dir <- "~/buckets/b1/datasets/"
 envg$EXPENV$arch_ambiente <- "miAmbiente.yml"
