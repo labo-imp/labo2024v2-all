5fd59555c47cb1044457b0661ffd57012293df39
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-11-11 12:06:51 +0000

    auto commit 30       20241111 120640

--- src/workflows/519_workflow_base_f202109_conc.r
+++ src/workflows/519_workflow_base_f202109_conc.r
@@ -319,33 +319,38 @@ HT_tuning_base <- function( pinputexps, bo_iteraciones, bypass=FALSE)
   #  los que tienen un vector,  son los que participan de la Bayesian Optimization
 
   param_local$lgb_param <- list(
-    boosting = "gbdt",
+    boosting = "gbdt", # puede ir  dart  , ni pruebe random_forest
     objective = "binary",
     metric = "custom",
     first_metric_only = TRUE,
     boost_from_average = TRUE,
     feature_pre_filter = FALSE,
-    force_row_wise = TRUE,
+    force_row_wise = TRUE, # para reducir warnings
     verbosity = -100,
-    max_bin = 31L,
-    num_iterations = 9999,
+    max_depth = -1L, # -1 significa no limitar,  por ahora lo dejo fijo
+    min_gain_to_split = 0.0, # min_gain_to_split >= 0.0
+    min_sum_hessian_in_leaf = 0.001, #  min_sum_hessian_in_leaf >= 0.0
+    lambda_l1 = 0.0, # lambda_l1 >= 0.0
+    lambda_l2 = 0.0, # lambda_l2 >= 0.0
+    max_bin = 31L, # lo debo dejar fijo, no participa de la BO
+    num_iterations = 9999, # un numero muy grande, lo limita early_stopping_rounds
+    
+    bagging_fraction = 1.0, # 0.0 < bagging_fraction <= 1.0
     pos_bagging_fraction = 1.0, # 0.0 < pos_bagging_fraction <= 1.0
     neg_bagging_fraction = 1.0, # 0.0 < neg_bagging_fraction <= 1.0
     is_unbalance = FALSE, #
-    extra_trees = FALSE,
-    max_depth = -1L,
+    scale_pos_weight = 1.0, # scale_pos_weight > 0.0
     
-    # Hiperparámetros optimizables (10 en total)
-    min_gain_to_split = c(0.0, 1.0),
-    lambda_l1 = c(0.0, 100.0),
-    lambda_l2 = c(0.0, 1000.0),
-    num_leaves = c(20L, 312L, "integer"),
-    learning_rate = c(0.01, 0.1),
-    min_data_in_leaf = c(200L, 2000L, "integer"),
-    bagging_fraction = c(0.6, 0.9),
-    scale_pos_weight = c(75L, 165L, "integer"),
-    feature_fraction = c(0.5, 0.9),
-    min_sum_hessian_in_leaf = c(0.001, 5.0)
+    drop_rate = 0.1, # 0.0 < neg_bagging_fraction <= 1.0
+    max_drop = 50, # <=0 means no limit
+    skip_drop = 0.5, # 0.0 <= skip_drop <= 1.0
+    
+    extra_trees = FALSE,
+    # Parte variable
+    learning_rate = c( 0.01, 0.2 ),
+    feature_fraction = c( 0.5, 0.9 ),
+    num_leaves = c( 8L, 20048L,  "integer" ),
+    min_data_in_leaf = c( 20L, 20000L, "integer" )
   )
 
 
@@ -440,7 +445,7 @@ wf_septiembre <- function( pnombrewf )
   #CN_canaritos_asesinos_base(ratio=0.2, desvio=4.0)
 
   ts9 <- TS_strategy_base9()
-  ht <- HT_tuning_base( bo_iteraciones = 200 )  # iteraciones inteligentes
+  ht <- HT_tuning_base( bo_iteraciones = 500 )  # iteraciones inteligentes
 
   fm <- FM_final_models_lightgbm( c(ht, ts9), ranks=c(1), qsemillas=20 )
   SC_scoring( c(fm, ts9) )
