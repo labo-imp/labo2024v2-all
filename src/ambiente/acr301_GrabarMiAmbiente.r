097a05aae6a7e77a025735df7c0f0ef089696e51
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-11-13 01:40:34 +0000

    auto commit 50       20241113 014024

--- src/ambiente/acr301_GrabarMiAmbiente.r
+++ src/ambiente/acr301_GrabarMiAmbiente.r
@@ -6,19 +6,19 @@ require("yaml")
 
 miAmbiente <- list()
 
-miAmbiente$modalidad <-  "conceptual"
-# miAmbiente$modalidad <-  "vivencial"
+#miAmbiente$modalidad <-  "conceptual"
+miAmbiente$modalidad <-  "vivencial"
 
-miAmbiente$dataset_pequeno <- "~/datasets/conceptual_dataset_pequeno.csv"
-# miAmbiente$dataset_pequeno <- "~/datasets/vivencial_dataset_pequeno.csv"
+#miAmbiente$dataset_pequeno <- "~/datasets/conceptual_dataset_pequeno.csv"
+miAmbiente$dataset_pequeno <- "~/datasets/vivencial_dataset_pequeno.csv"
 
 
-miAmbiente$dataset_competencia <- "~/datasets/conceptual_competencia_2024.csv.gz"
-# miAmbiente$dataset_competencia <- "~/datasets/vivencial_competencia_2024.csv.gz"
+#miAmbiente$dataset_competencia <- "~/datasets/conceptual_competencia_2024.csv.gz"
+miAmbiente$dataset_competencia <- "~/datasets/vivencial_competencia_2024.csv.gz"
 
 
 # aqui va su primer semilla
-miAmbiente$semilla_primigenia <- 102191L
+miAmbiente$semilla_primigenia <- 892249L
 
 
 write_yaml( miAmbiente,
@@ -31,6 +31,6 @@ dir.create("~/buckets/b1/repos/", showWarnings = FALSE)
 dir.create("~/buckets/b1/repos/labo2024ba", showWarnings = FALSE)
 
 cat("Espere unos minutos mientras se hace un backup\n")
-system( "~/install/repobrutalcopy.sh", wait= FALSE )
+system( "~/install/repobrutalcopy.sh" )
 
 cat("\nel ambiente ha sido GRABADO\n")
