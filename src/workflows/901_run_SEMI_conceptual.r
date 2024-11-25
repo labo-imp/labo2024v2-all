71738898cb031ea0b118fcd3de2030c52b447328
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-11-25 23:36:49 +0000

    auto commit 38       20241125 233638

--- src/workflows/901_run_SEMI_conceptual.r
+++ src/workflows/901_run_SEMI_conceptual.r
@@ -1,7 +1,7 @@
 require("rlang")
 
 # workflow que voy a correr
-PARAM <- "src/workflows/991_workflow_SEMI_conceptual_Model_010.r"
+PARAM <- "src/workflows/991_workflow_SEMI_conceptual_Model_012.r"
 
 envg <- env()
 
